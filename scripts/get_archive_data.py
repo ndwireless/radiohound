81f2d1cec326ee85a2336f1f77ccbd028a6990f8
Randy Herban <rherban@nd.edu>
2023-06-15 16:07:57 -0400

    Example scripts for downloading and parsing archived scan data from server.

---
+++ scripts/get_archive_data.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''
+Downloads all scan data for given node and date range.  Scans are saved as individual json files in a directory named 'output'.
+You will need to contact ND staff to be allowed through the firewall.
+
+Author: Randy Herban @ Notre Dame
+'''
+import paho.mqtt.client as mqtt
+import queue
+import argparse
+import time
+import simplejson as json
+import numpy
+import base64
+import ipaddress
+import requests
+
+parser = argparse.ArgumentParser(description='RadioHound Scan')
+parser.add_argument('--node', type=str)
+parser.add_argument('--start_date', type=str)
+parser.add_argument('--end_date', type=str)
+args = parser.parse_args()
+
+api = f'http://radiohound.ee.nd.edu:5000/archive?node_id={args.node}&start_date={args.start_date}&end_date={args.end_date}'
+results = requests.get(api).json()
+for result in results:
+    data_id = result['data_id']
+    fmin = result['fmin']
+    mac_address = result['mac_address']
+    timestamp = result['timestamp'].replace(' ', '_')
+    #ip_address = result['IP_addr'].replace('192.168.7.2','').replace('192.168.6.2','').rstrip().split(' ')[0]
+    print(f"Found scan {data_id} {timestamp}")
+    data_request = f'http://radiohound.ee.nd.edu:5000/data?id={data_id}'
+    data = requests.get(data_request).json()
+    print(f"Data: {data}")
+    file = open(f"output/{data_id}-{mac_address}-{timestamp}.json",'w')
+    file.write(json.dumps(data))
+    file.close()
+
---
+++ scripts/parse_scan.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+'''
+ Parses a downloaded RadioHound scan file
+
+ Should be ran with the filename as input:
+ python parse_scan.py sample.json
+
+ Author: Randy Herban @ Notre Dame
+'''
+import base64
+import json
+import numpy
+import sys
+
+file = sys.argv[1]
+with open(file,'r') as handle:
+    input = json.load(handle)
+
+dt = numpy.dtype(input["type"])
+data = numpy.frombuffer(base64.b64decode(input["data"]), dtype=dt)
+print("data: " + str(data))
+print("len(data): " + str(len(data)))
+freq_array = numpy.linspace(input['metadata']['fmin'], input['metadata']['fmax'], input['metadata']['nfft'])
+dbm = 10*numpy.log10(data) + 30
+print("dbm values:" + str(dbm))
+
+max = numpy.max(dbm)
+print("max power is " + str(max) + " at " + str(freq_array[numpy.where(dbm == max)]/1e6) + " MHz")
+
+
+
+
