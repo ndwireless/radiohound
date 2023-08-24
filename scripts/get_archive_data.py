ac35faf16ea11af0b0b40d390453bb873efef3a8
Randy Herban <rherban@nd.edu>
2023-08-24 13:53:23 -0400

    Update usage documentation.

--- scripts/get_archive_data.py
+++ scripts/get_archive_data.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python3
 '''
-Downloads all scan data for given node and date range.  Scans are saved as individual json files in a directory named 'output'.
-You will need to contact ND staff to be allowed through the firewall.
+Downloads all scan data for given node or experiment and date range.  You will need to contact ND staff to be allowed through the firewall.
+
+python3 get_archive_data.py --node (MAC_ADDRESS|EXPERIMENT_ID) --start_date 'YYYY-MM-DD HH:MM:SS' --end_date 'YYYY-MM-DD HH:MM:SS'
+
+The mac address can be specified with or without colons and they'll be automatically stripped off.  
+Experiqment ID will be in the form of "experiment_NNNN" with the numeric ID from the website.  
+Dates can also be specified as: YYYY-MM-DD or YYYY-MM-DD HH:MM.  
+
+Scans are saved as individual json files in a directory named 'output'.  To understand the format, look at the parse_scan.py file in this repository.  
 
 Author: Randy Herban @ Notre Dame
 '''
 import paho.mqtt.client as mqtt
-import queue
 import argparse
-import time
 import simplejson as json
-import numpy
-import base64
-import ipaddress
 import requests
 import os
 import sys
 import re
 
 parser = argparse.ArgumentParser(description='RadioHound Scan')
-parser.add_argument('--node', type=str)
-parser.add_argument('--start_date', type=str, help='YYYY-MM-DDTHH:MM:SS')
-parser.add_argument('--end_date', type=str, help='YYYY-MM-DDTHH:MM:SS')
+parser.add_argument('--node', type=str, help="Mac address of the target node, with or without colons.  Or Experiment ID: experiment_NNNN")
+parser.add_argument('--start_date', type=str, help='YYYY-MM-DD HH:MM:SS')
+parser.add_argument('--end_date', type=str, help='YYYY-MM-DD HH:MM:SS')
 args = parser.parse_args()
 if not os.path.isdir('output'):
   os.mkdir('output')
 
+args.node = args.node.replace(':', '')
 args.start_date = args.start_date.replace(' ', 'T')
 args.end_date = args.end_date.replace(' ', 'T')
 # Auto-append time if not entered
